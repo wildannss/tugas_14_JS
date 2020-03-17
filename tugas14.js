961209b2f15fc6a69d19c56cb0d00ebd9fe306f4
wildan <wildan.syarif1021@gmail.com>
2020-03-17 13:22:22 +0700

    tugas niomic

---
+++ tugas14.js
@@ -0,0 +1,10 @@
+function tugas14(){
+    let tanggal = 1991-08-08;
+    tanggalbagi = tanggal / 37
+    console.log(tanggalbagi);
+    console.log(Math.ceil(tanggalbagi));
+    console.log(Math.floor(tanggalbagi));
+    console.log(Math.round(tanggalbagi));
+};
+
+tugas14();
